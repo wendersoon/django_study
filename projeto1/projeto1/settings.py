c6bb6c560873bba799eee481b541921f52a23e3a
wenderson <ds.wendersonmelo@gmail.com>
2023-10-16 18:05:58 -0300

    models

--- projeto1/projeto1/settings.py
+++ projeto1/projeto1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'core',                         # É preciso adicionar a aplicação aqui para que o projeto django reconheça
 ]
 
 MIDDLEWARE = [
