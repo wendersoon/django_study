3b2f5391f1da0bab3b3f2216b418decf5afc6329
wenderson <ds.wendersonmelo@gmail.com>
2023-10-17 16:05:25 -0300

    Adicionei um começo de conexao

--- projeto1/core/views.py
+++ projeto1/core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 
 def index(request):
+    produtos = Produto.objects.all()    
     context = {
         'Aluno': 'Batman',
         'Curso': 'Das Trevas',
-        'Grau': 'Cavaleiro'
+        'Grau': 'Cavaleiro',
+        'produtos': produtos,
     }
     return render(request, 'index.html', context)
 
