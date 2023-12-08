51bf44db0f3afd7e7599dc27836175a04ff5bac6
wenderson <ds.wendersonmelo@gmail.com>
2023-12-07 22:36:25 -0300

    criação dos templates, views completa

--- realtime/chat/views.py
+++ realtime/chat/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from typing import Any
+from django.views.generic import TemplateView
+from django.utils.safestring import mark_safe
+import json
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'sala.html'
+
+class SalaView(TemplateView):
+    template_name = 'sala.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(SalaView, self).get_context_data(**kwargs)
+        context['nome_sala_json'] = mark_safe(
+            json.dumps(self.kwargs['nome_sala'])
+        )
+        return context
+        
+            
+    
